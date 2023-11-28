8434f2612628add56c407e830f216849151ae730
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-27 23:51:01 -0400

    Adding vitest dependency and creating first test fizzbuzz to prove it works properly before doing unit testing

---
+++ src/test/fizzbuzz.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+
+const fizzbuzz = (num) => {
+    
+}
+
+describe('fizzbuzz', ()  => {
+
+    it('should be a function', () => {
+        expect(typeof fizzbuzz).toBe('function');
+    })
+})
