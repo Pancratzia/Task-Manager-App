27fd651c5b2de09107d40a9ef9a0845c3d122e6d
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-27 20:13:58 -0400

    Starting with filters after finishing project along with pagination

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,13 +10,13 @@ const Form = () => {
     name: "",
     description: "",
     priority: "low",
-    status: 0, /* 0: pending, 1: completed */ 
+    status: 0 /* 0: pending, 1: completed */,
   });
 
   useEffect(() => {
     if (id) {
       const tasksInStorage = JSON.parse(localStorage.getItem("tasks")) || [];
-      const existingTask = tasksInStorage.find((t) => t.id);
+      const existingTask = tasksInStorage.find((t) => t.id === id);
 
       if (existingTask) {
         setTask(existingTask);
@@ -38,11 +38,14 @@ const Form = () => {
 
     if (id) {
       const updatedTasks = tasksInStorage.map((t) =>
-        t.id === parseInt(id) ? { ...t, ...task } : t
+        t.id === id ? { ...t, ...task } : t
       );
       localStorage.setItem("tasks", JSON.stringify(updatedTasks));
     } else {
-      const taskId = uuid(); 
+      let taskId;
+      do{
+        taskId = uuid();
+      }while(tasksInStorage.find((t) => t.id === taskId));
       const newTask = { id: taskId, ...task };
       const updatedTasks = [...tasksInStorage, newTask];
       localStorage.setItem("tasks", JSON.stringify(updatedTasks));
@@ -55,63 +58,66 @@ const Form = () => {
       navigate("/");
     }
   };
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTask((prevTask) => ({ ...prevTask, [name]: value }));
   };
 
   return (
-    <>
-      <h2>
-        {id ? "Edit Task" : "Create Task"}
-      </h2>
+    <div className="form__container">
+      <h2 className="form__title">{id ? "Edit Task" : "Create Task"}</h2>
 
-      <Link to="/">
-        Back to List
-      </Link>
+      <div className="form__btn">
+        <Link className="btn btn--secondary" to="/">
+          Back to List
+        </Link>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Task Name</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Task Name"
-            value={task.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <fieldset className="form__fieldset">
+          <legend className="form__legend">Task Details</legend>
+          <div className="form__group">
+            <label className="form__label" htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Task Name"
+              value={task.name}
+              onChange={handleChange}
+              className="form__input form__input--text"
+              required
+            />
+          </div>
 
-        <div>
-          <label htmlFor="description">Task Description</label>
-          <textarea
-            name="description"
-            placeholder="Task Description"
-            value={task.description}
-            onChange={handleChange}
-          ></textarea>
-        </div>
+          <div className="form__group">
+            <label className="form__label" htmlFor="description">Description</label>
+            <textarea
+              name="description"
+              placeholder="Task Description"
+              value={task.description}
+              onChange={handleChange}
+              className="form__input form__input--textarea"
+            ></textarea>
+          </div>
 
-        <div>
-          <label htmlFor="status">Priority Level</label>
-          <select
-            name="priority"
-            value={task.priority}
-            onChange={handleChange}
-          >
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
-          </select>
-        </div>
-
-        <button type="submit">Submit</button>
+          <div className="form__group">
+            <label className="form__label" htmlFor="status">Priority Level</label>
+            <select
+              name="priority"
+              value={task.priority}
+              onChange={handleChange}
+              className="form__input form__input--select"
+            >
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+        </fieldset>
+        <button className="btn btn--primary" type="submit">Submit</button>
       </form>
-    </>
+    </div>
   );
 };
 
 export default Form;
-
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,11 +4,32 @@ import Task from "./Task";
 
 const List = () => {
   const [tasks, setTasks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const tasksPerPage = 5;
+
+  const getTasksForPage = () => {
+    const startIndex = (currentPage - 1) * tasksPerPage;
+    const endIndex = startIndex + tasksPerPage;
+    return tasks.slice(startIndex, endIndex);
+  };
+
+  const goToNextPage = () => {
+    if ((currentPage - 1) * tasksPerPage + tasksPerPage < tasks.length) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const goToPrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
 
   useEffect(() => {
     const tasksInStorage = JSON.parse(localStorage.getItem("tasks")) || [];
     setTasks(tasksInStorage);
-  }, []); 
+  }, []);
 
   const handleCheckboxChange = (taskId) => {
     const updatedTasks = tasks.map((task) =>
@@ -26,31 +47,96 @@ const List = () => {
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
   };
 
+  const tasksToDisplay = getTasksForPage();
+
   return (
     <div className="list">
-
       <div className="list__btn">
-        <Link to="/create" className="btn btn--primary">Create Task</Link>
+        <Link to="/create" className="btn btn--primary">
+          Create Task
+        </Link>
       </div>
 
       <h2 className="list__title">Task List</h2>
-      <p className="list__subtitle">Mark a task as completed by clicking on their respective checkbox</p>
+      <p className="list__subtitle">
+        Mark a task as completed by clicking on their respective checkbox
+      </p>
+      <p className="list__subtitle">Edit a task by clicking on it's name</p>
 
       <ul className="list__priorities">
         <li className="list__priority list__priority--high">High</li>
         <li className="list__priority list__priority--medium">Medium</li>
         <li className="list__priority list__priority--low">Low</li>
       </ul>
-      
+
+      <h4 className="list__filters-title">Filters</h4>
+      <div className="filters">
+
+        <div className="filter">
+          <p className="filter__title">Status</p>
+          <ul className="filter__list">
+            <li className="filter__item filter__item--completed">
+              <input type="checkbox" name="completed" /> Completed
+            </li>
+            <li className="filter__item filter__item--pending">
+              <input type="checkbox" name="pending" /> Pending
+            </li>
+          </ul>
+        </div>
+
+        <div className="filter">
+          <p className="filter__title">Priority</p>
+          <ul className="filter__list">
+            <li className="filter__item filter__item--high">
+              <input type="checkbox" name="high" /> High
+            </li>
+            <li className="filter__item filter__item--medium">
+              <input type="checkbox" name="medium" /> Medium
+            </li>
+            <li className="filter__item filter__item--low">
+              <input type="checkbox" name="low" /> Low
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
       {tasks.length === 0 ? (
         <p className="list__empty">No tasks available</p>
       ) : (
         <ul>
-          {tasks.map((task) => (
-            <Task key={task.id} task={task} handleCheckboxChange={handleCheckboxChange} handleDeleteButtonClick={handleDeleteButtonClick} />
+          {tasksToDisplay.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              handleCheckboxChange={handleCheckboxChange}
+              handleDeleteButtonClick={handleDeleteButtonClick}
+            />
           ))}
         </ul>
       )}
+
+      <div className="pagination">
+        <button
+          className="pagination__btn"
+          onClick={goToPrevPage}
+          disabled={currentPage === 1}
+        >
+          &lt;
+        </button>
+        <span className="pagination__page">
+          {currentPage + "/" + Math.ceil(tasks.length / tasksPerPage)}
+        </span>
+        <button
+          className="pagination__btn"
+          onClick={goToNextPage}
+          disabled={
+            (currentPage - 1) * tasksPerPage + tasksPerPage >= tasks.length
+          }
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 };
